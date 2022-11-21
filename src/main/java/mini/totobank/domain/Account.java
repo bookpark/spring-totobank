3c5f855f11088e0db00e19f1bfd6cbfe07ebfd2a
bookpark <bkbkgg@gmail.com>
2022-11-21 17:23:33 +0900

    accountNumber 추가

--- src/main/java/mini/totobank/domain/Account.java
+++ src/main/java/mini/totobank/domain/Account.java
@@ -15,6 +15,9 @@ public class Account {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "account_number")
+    private String accountNumber;
+
     @Column
     private String name;
 
