9c1f7c15011f856c2f0e878ccd6df68967787bdc
bookpark <bkbkgg@gmail.com>
2022-11-21 15:28:04 +0900

    db 테스트

--- src/test/java/mini/totobank/TotobankApplicationTests.java
+++ src/test/java/mini/totobank/TotobankApplicationTests.java
@@ -1,13 +1,30 @@
 package mini.totobank;
 
+import mini.totobank.domain.Account;
+import mini.totobank.repository.AccountRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class TotobankApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Test
+    void addAccount() {
+        Account account = new Account();
+        account.setName("박부기");
+        account.setGrade("VIP");
+        account.setBalance(0);
+    }
+
+    @Test
+    void contextLoads() {
+    }
 
 }
