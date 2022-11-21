6d1bec6b5fa6d23277e3d4ddab4f3827c2c1e916
bookpark <bkbkgg@gmail.com>
2022-11-21 17:25:04 +0900

    reformat code

--- src/main/java/mini/totobank/service/AccountService.java
+++ src/main/java/mini/totobank/service/AccountService.java
@@ -1,16 +1,14 @@
 package mini.totobank.service;
 
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import mini.totobank.domain.Account;
 import mini.totobank.repository.AccountRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AccountService {
-    
+
     private final AccountRepository accountRepository;
 
     public void makeAccount(Account acc) {
