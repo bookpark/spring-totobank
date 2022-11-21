5d4a5b841ef60f707a7583178ef23cc2d5ea03a5
bookpark <bkbkgg@gmail.com>
2022-11-21 13:45:46 +0900

    initial commit

---
+++ src/main/java/mini/totobank/TotobankApplication.java
@@ -0,0 +1,13 @@
+package mini.totobank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotobankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotobankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mini/totobank/TotobankApplicationTests.java
@@ -0,0 +1,13 @@
+package mini.totobank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotobankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
